15a777f1c9f03a8ce657d4e07ed0c1c3bd02e959
cl <201620181102@ecut.edu.cn>
2021-02-23 16:25:35 +0800

    第一次修改

--- git/src/main/java/com/sc/git/Hello.java
+++ git/src/main/java/com/sc/git/Hello.java
@@ -1,12 +1,15 @@
 package com.sc.git;
 
 public class Hello {
-
     private int i = 10;
-    String name="myc";
-    int c=1;
+
     public static void main(String[] args) {
 
+        String name="myc";
+        String sc="myc1号";
+        int c=1;
+        System.out.println("欢迎来到github");
     }
+
 
 }
